b052cafc9a47991d913a0e9cac668e4241c6b036
Nicholas Allicock <nicholasallicock@outlook.com>
2021-06-22 12:02:09 -0400

    services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'servicess', component: ServicesComponent}
+  {path: 'services', component: ServicesComponent}
 ];
 
 @NgModule({
